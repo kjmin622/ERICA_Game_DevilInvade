29145c8a3be76d695eeb50c06f6963ccf140c058
kjmin622 <kjmin622@naver.com>
2019-11-06 20:53:51 +0900

    GUI.py update

--- src/GUI.py
+++ src/GUI.py
@@ -1,16 +1,19 @@
 import pygame as pg
 pg.init()
-
-size = [720, 580]
-
+size = [720, 640]
 screen = pg.display.set_mode(size)
-
 pg.display.set_caption("Devil Invade") #game title
-
 done = False
 clock = pg.time.Clock()
 
-white = (255,255,255)
+white = (255,255,255) #기본 색깔
+black = (0,0,0)
+
+#image = pg.image.load("../image/enemy/mob1.png").convert_alpha()
+# 이미지 예시
+
+#pg.draw.rect(screen, 색깔, [x,y,x크기, y크기], 각도)
+# 직사각형 그리기
 
 while not done :
 
@@ -21,6 +24,12 @@ while not done :
             done = True
     
     screen.fill(white)
+
+    #인터페이스, 추후에 이미지로 바꾸기
+    pg.draw.rect(screen, black, [0,0,720,100], 0)
+    pg.draw.rect(screen, black, [0,580,720,60], 0)
+    
+
 
 
 
