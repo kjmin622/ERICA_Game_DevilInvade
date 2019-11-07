22ef3e4e1a6ab95ffbcc7c4b0b55c97f14d05b87
kjmin622 <kjmin622@naver.com>
2019-11-08 01:00:17 +0900

    Player.py 생성자 추가

---
+++ src/Player.py
@@ -0,0 +1,11 @@
+
+class Player :
+    hp = 10         #체력, 초기값 10
+    power = 1       # 공격력, 초기값 1
+    move_speed = 1  # 이동속도, 초기값 1
+    at_speed = 1    # 공격속도, 초기값 1
+    
+    def __init__(self):
+        print("player_init")
+
+
