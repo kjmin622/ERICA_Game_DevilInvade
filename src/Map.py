9d6709a099e961e89abca4726ab4498aef14f6bd
huskycat1202 <annie991202@naver.com>
2019-11-13 21:23:52 +0900

    make enter_door

--- src/Map.py
+++ src/Map.py
@@ -1,6 +1,8 @@
 from enemy import Mob1
 from MapList import *
 import random
+import Crash
+from Player import *
 
 
 class Room:
@@ -21,6 +23,39 @@ class Room:
 
     def get_e_list(self):
         return self.e_list
+
+    def enter_door(self, player,e_list):
+        if(not e_list):
+            return -1
+
+        d0 = [300,70]
+        d2 = [70,200]
+        d1 = [300,450]
+        d3 = [650,200]
+
+        if(Crash.One(player.get_x(), player.get_y(), d0[0], d0[1], player.get_width(), player.get_height(), 50,0)
+                and self.door[0]):
+            return 0
+
+        if(Crash.One(player.get_x(), player.get_y(), d1[0], d1[1], player.get_width(), player.get_height(), 50,0)
+                and self.door[1]):
+            return 1
+    
+        if(Crash.One(player.get_x(), player.get_y(), d2[0], d2[1], player.get_width(), player.get_height(), 0,50)
+                and self.door[2]):
+            return 2
+
+        if(Crash.One(player.get_x(), player.get_y(), d3[0], d3[1], player.get_width(), player.get_height(), 0,50)
+                and self.door[3]):
+            return 3
+        
+        return -1
+
+
+
+
+
+
 
 
 
